991733f51a89063b0c41dd14c06a516e2c2ee1ec
Atindra Singh Rathore <118996145+AtindraRathore@users.noreply.github.com>
2023-12-30 22:16:06 +0530

    Add files via upload
    ok

---
+++ contest 3/script.js
@@ -0,0 +1,27 @@
+var arr = [
+    {
+        title: "veg",
+        item_name:"Russain salad",
+        time:"41 min"
+    },
+    {
+        title:"veg",
+        item_name: "shahi korma",
+        time:"30 min"
+    },
+    {
+        title: "non veg",
+        item_name: "chicken kabab",
+        time: "20 min"
+    },
+    {
+        title: "veg",
+        item_name: "dal makhni",
+        time:"14 min"
+    },
+    {
+        title: "non veg",
+        item_name: "butter chicken",
+        time: "43 min"
+    }
+];
